b9e84ab9f8e0d26192cc53e8b3aec073e4df6cbd
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-07-13 16:37:17 +0800

    add Variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+var petdog = "Rex"
+var petCat = "Pepper"
+console.log(petdog)
+console.log(petCat)
+console.log("My pet dog's name is : ", petdog)
+console.log("My pet cat's name is : ", petCat)
+var catSound = "purr"
+var dogSound = "woof"
+console.log(petdog, " says " , dogSound)
+console.log(petCat, " says " ,catSound)
+catSound = "meow"
+console.log(petCat,  " now says ",  catSound)
+
